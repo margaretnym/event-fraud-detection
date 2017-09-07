a56a19614ae214f8518ecbea1ad625bb66211dbc
Alexandra Magaña Noronha <magananoronha@gmail.com>
2017-09-07 13:08:12 -0500

    thinks this should work

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import pickle
+import sleep
 from build_model import TextClassifier
 from pymongo import MongoClient
 from sklearn.ensemble import GradientBoostingClassifier
@@ -17,18 +18,16 @@ if __name__ == '__main__':
     db = client['my_db']
     hash_table = db['hashes']
     data_table = db['data']
-    hash_table = db['hashes']
-    data_table = db['data']
 
-    new_data = request_data_point()
-    new_data = json.loads(new_data)
-
-    data_hash = generate_hash(new_data)
-
-    if check_duplicate(data_hash, 'data_hash' hash_table) == False:
-        hash_table.insert_one({'data_hash' : data_hash})
-        new_data = json.loads(new_data)
-        prediction = predict_fraud(X)
-        new_data['fraud'] = prediction
-
+    while True:
+        sleep(10)
+        new_data = request_data_point()
+        data_hash = generate_hash(new_data)
+        if check_duplicate(data_hash, 'data_hash' hash_table) == False:
+            hash_table.insert_one({'data_hash' : data_hash})
+            new_data = json.loads(new_data)
+            prediction = predict_fraud(X)
+            new_data['fraud'] = prediction
+            data_table.insert_one(new_data)
+            
     app.run(host='0.0.0.0', debug=True)
